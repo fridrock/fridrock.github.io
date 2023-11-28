10f58175b8be17c0c9e687a1b0f0dec4c6908080
fridrock <fridrix.aleksandr.2004@mail.ru>
2023-11-28 15:30:56 +0300

    started exersize page

---
+++ js/exersize.js
@@ -0,0 +1,21 @@
+const userInput = document.querySelector('.userInput');
+const submitExersize = document.querySelector('.answer > button');
+
+function checkAnswer(answer){
+    const correctResult = `db.insert({name:"Aleksandr", surname:"Fridrikh"});`;
+    let output = ""
+    let outputStatus = 0;
+    if(answer === correctResult){
+        outputStatus = 1;
+        output = `Успешное завершение программы, пользователь {name:
+            "Aleksandr", surname:"Fridrikh"} был добавлен в базу данных.`
+    }else{
+        output = "Что-то пошло не так";
+    }
+    return [outputStatus, output];
+}
+submitExersize.addEventListener('click', (e)=>{
+    e.preventDefault();
+    let answerValue = userInput.value;
+    let checkResult = checkAnswer(answerValue);
+})
